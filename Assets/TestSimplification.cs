57b1a03dccdff82705ed401913398c2846db7968
nnakata <nakata@team-lab.com>
2013-11-01 15:51:01 +0900

    テスト

--- Assets/TestSimplification.cs
+++ Assets/TestSimplification.cs
@@ -18,23 +18,20 @@ public class TestSimplification : MonoBehaviour {
 	}
 	
 	void Update() {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButton(0))
 			Test02(isoSphere.mesh);
-		}
 	}
 
 	void Test02 (Mesh sphere) {
 		var edgeCost = _simp.costs.RemoveFront();
-		Debug.Log(string.Format("{0},{1}->{2}", 
-			sphere.vertices[edgeCost.edge.v0], sphere.vertices[edgeCost.edge.v1], edgeCost.minPos));		
 
 		_simp.CollapseEdge(edgeCost);
 		
 		Vector3[] vertices;
 		int[] triangles;
 		_simp.ToMesh(out vertices, out triangles);
-		Debug.Log(string.Format("nVert({0}->{1}),nTri({2}->{3})", sphere.vertices.Length, vertices.Length, sphere.triangles.Length / 3, triangles.Length / 3));
-
+		
+		Debug.Log(string.Format("Vertex {0}->{1} Face {2}->{3}", sphere.vertices.Length, vertices.Length, sphere.triangles.Length / 3, triangles.Length / 3));
 		
 		sphere.Clear();
 		sphere.vertices = vertices;
